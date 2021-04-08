a1756ab899e40bc759379bb9125e46cfcdbbb341
[._.]/ Adam Eivy <70015+atomantic@users.noreply.github.com>
2021-04-07 21:49:57 -0700

    code cleanup

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,10 +1,10 @@
 <template lang="pug">
   .home
     row(container, :gutter="12")
-      column.gallery(v-for="(value, name) in galleries", :xs="6", :s="6", :md="4", :lg="3", :xl="4")
+      column.gallery(v-for="(value, name) in thumbs", :xs="6", :s="6", :md="4", :lg="3", :xl="4")
         router-link(:to="`/${name}`")
-          img(v-if="galleryData[name].find(i=>i.objectId===`${value.main}`).token_info.formats[0].mimeType!=='video/mp4'", :src="`https://cloudflare-ipfs.com/ipfs/${galleryData[name].find(i=>i.objectId===`${value.main}`).token_info.artifactUri.replace('ipfs://','')}`")
-          video(v-if="galleryData[name].find(i=>i.objectId===`${value.main}`).token_info.formats[0].mimeType==='video/mp4'", :src="`https://ipfs.io/ipfs/${galleryData[name].find(i=>i.objectId===`${value.main}`).token_info.artifactUri.replace('ipfs://','')}`", loop, autoplay)
+          img(v-if="value.formats[0].mimeType!=='video/mp4'", :src="`https://cloudflare-ipfs.com/ipfs/${value.artifactUri.replace('ipfs://','')}`")
+          video(v-if="value.formats[0].mimeType==='video/mp4'", :src="`https://ipfs.io/ipfs/${value.artifactUri.replace('ipfs://','')}`", loop, autoplay)
           .title {{name.charAt(0).toUpperCase() + name.slice(1)}}
 </template>
 
@@ -14,6 +14,18 @@ export default {
   props: {
     galleryData: Object,
     galleries: Object
+  },
+  computed: {
+    // galleries is a human friendly CMS
+    // galleryData has all the meta info
+    // we need galleryData but the "main" thumb is stored in galleries
+    thumbs: function (){
+      const items = {};
+      Object.keys(this.galleries).forEach(name=>{
+        items[name] = this.galleryData[name].find(i=>i.objectId===`${this.galleries[name].main}`).token_info
+      })
+      return items
+    }
   }
 }
 </script>
