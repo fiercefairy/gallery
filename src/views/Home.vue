04810fa7fdec86a69d8c5dbea0aa2c64bc5de200
[._.]/ Adam Eivy <70015+atomantic@users.noreply.github.com>
2021-04-07 21:29:20 -0700

    fix home thumb for videos

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -3,8 +3,8 @@
     row(container, :gutter="12")
       column.gallery(v-for="(value, name) in galleries", :xs="6", :s="6", :md="4", :lg="3", :xl="4")
         router-link(:to="`/${name}`")
-          //- p {{galleryData[name].find(i=>i.objectId===`${value.main}`)}}
-          img(:src="`https://cloudflare-ipfs.com/ipfs/${galleryData[name].find(i=>i.objectId===`${value.main}`).token_info.artifactUri.replace('ipfs://','')}`")
+          img(v-if="galleryData[name].find(i=>i.objectId===`${value.main}`).token_info.formats[0].mimeType!=='video/mp4'", :src="`https://cloudflare-ipfs.com/ipfs/${galleryData[name].find(i=>i.objectId===`${value.main}`).token_info.artifactUri.replace('ipfs://','')}`")
+          video(v-if="galleryData[name].find(i=>i.objectId===`${value.main}`).token_info.formats[0].mimeType==='video/mp4'", :src="`https://ipfs.io/ipfs/${galleryData[name].find(i=>i.objectId===`${value.main}`).token_info.artifactUri.replace('ipfs://','')}`", loop, autoplay)
           .title {{name.charAt(0).toUpperCase() + name.slice(1)}}
 </template>
 
@@ -21,7 +21,7 @@ export default {
 <style lang="sass" scoped>
 .gallery
   text-align: center
-  img
+  img, video
     width:100%
     max-width: 256px
 </style>
