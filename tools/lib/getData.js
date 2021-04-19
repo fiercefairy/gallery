eb9adc70ac4ca8a6c98fbb46cc499c0a61318486
[._.]/ Adam Eivy <70015+atomantic@users.noreply.github.com>
2021-04-19 15:54:29 -0700

    handle errors in updating

--- tools/lib/getData.js
+++ tools/lib/getData.js
@@ -1,10 +1,26 @@
 const axios = require("axios");
+const sleep = require("./sleep");
 module.exports = async id => {
-  const response = await axios.post(
-    "https://51rknuvw76.execute-api.us-east-1.amazonaws.com/dev/objkt",
-    {
-      objkt_id: `${id}`
+  let retries = 0;
+  const attempt = async () => {
+    const res = await axios
+      .post(
+        "https://51rknuvw76.execute-api.us-east-1.amazonaws.com/dev/objkt",
+        {
+          objkt_id: `${id}`
+        }
+      )
+      .then(response => response)
+      .catch(async e => {
+        console.error(e.message);
+      });
+    if (!res && retries < 10) {
+      retries++;
+      await sleep(1000);
+      return attempt();
     }
-  );
-  return response.data.result;
+
+    return res ? res.data.result : null;
+  };
+  return attempt();
 };
